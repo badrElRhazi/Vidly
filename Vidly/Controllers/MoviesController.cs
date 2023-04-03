0b24fad599014710b9d1926179ffc44b30dd3756
Badr El Rhazi <rzi.badr@gmail.com>
2023-04-03 18:36:40 +0000

    x commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,10 +14,17 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
-           var viewResult=new ViewResult();
-            viewResult.ViewData.Model = movie; 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
 
         }
@@ -30,10 +38,10 @@ namespace Vidly.Controllers
             if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)")]
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseYear(int year, Byte month)
         {
-            return Content(year+ "/" +month);
+            return Content(year + "/" + month);
         }
     }
 }
