461d6bcc243690742a1f269d330c1d00d85903c8
Badr El Rhazi <rzi.badr@gmail.com>
2023-04-04 01:27:35 +0000

    sixth commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1,Name="Shrek"},
+                new Movie{Id=2,Name="The Prisoners"}
+            };
+        }
         // GET: Movies /Random
         public ActionResult Random()
         {
@@ -44,4 +57,4 @@ namespace Vidly.Controllers
             return Content(year + "/" + month);
         }
     }
-}
+} 
---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+            public DbSet Movies { get; set; }
+            public DbSet Customers { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            }
+        }
+    }
